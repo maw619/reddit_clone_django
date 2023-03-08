5c73743519d0a8ccb1b95ce340ba50864b09f601
maw619 <marcowolff619@gmail.com>
2023-03-07 20:17:49 -0500

    removed saving to database

--- reddit_api/views.py
+++ reddit_api/views.py
@@ -3,16 +3,10 @@ import praw
 from requests.auth import HTTPBasicAuth
 import requests
 from .models import RedditAPI
-
+from django.http import JsonResponse
 
 def reddit_home(request):
     redall = RedditAPI.objects.all()
-    loadRedditApi()
- 
-    context = {'data': redall}
-    return render(request, 'reddit/index.html', context)
-
-def loadRedditApi():
     reddit = praw.Reddit(
         client_id="bOtmWr8d79ph7Z5A-h34BQ",
         username="maw329",
@@ -20,16 +14,17 @@ def loadRedditApi():
         user_agent="my user agent",
     )
 
-    red = RedditAPI()
- 
-    for submission in reddit.subreddit("destiny").hot(limit=10): 
-        red.title = submission.title
-        red.url = submission.url
-        red.author = submission.author
-        red.author_fullname = submission.author_fullname
-        red.domain = submission.domain 
-        
-        
-        print(type(red))
-        print(red)
-        red.save()
+    posts = []
+    for post in reddit.subreddit("destiny").hot(limit=10):
+        posts.append({
+            'title': post.title,
+            'url': post.url,
+            'score': post.score,
+            'author': post.author.name,
+        })
+   
+    
+    context = {'data': posts}
+    return render(request, 'reddit/index.html', context)
+    return JsonResponse({'posts': posts})
+
